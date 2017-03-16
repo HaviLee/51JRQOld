b91e21b5407bfe6dabcf93711f9957ef3eac6dcd
Havi_Lee <1058695900@qq.com>
2017-03-16 23:58:28 +0800

    build

--- 51JRQ/CompanyShow/CompanyshowView.m
+++ 51JRQ/CompanyShow/CompanyshowView.m
@@ -9,10 +9,17 @@
 #import "CompanyshowView.h"
 #import "UIView+MIPipeline.h"
 #import "CompanyshowPipeline.h"
+#import "CompanyJobTableViewCell.h"
+#import "CompanyHeaderView.h"
+#import "CompanySubHeaderView.h"
+#import "MJRefresh.h"
 
-@interface CompanyshowView ()
+@interface CompanyshowView ()<UITableViewDelegate,UITableViewDataSource>
 
 @property (nonatomic, strong) CompanyshowPipeline *pipeline;
+@property (nonatomic, strong) UITableView *jobShowTableView;
+@property (nonatomic, strong) CompanyHeaderView *headerBackView;
+@property (nonatomic, strong) CompanySubHeaderView *subView;
 
 @end
 
@@ -21,9 +28,87 @@
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
+        self.backgroundColor = [UIColor clearColor];
+        [self addSubview:self.jobShowTableView];
+        self.jobShowTableView.mj_header = [MJRefreshNormalHeader headerWithRefreshingTarget:self refreshingAction:@selector(loadNewData)];
 
+            // Enter the refresh status immediately
+        [self.jobShowTableView.mj_header beginRefreshing];
     }
     return self;
+}
+
+#pragma setter meathod
+
+- (CompanyHeaderView*)headerBackView
+{
+    if (!_headerBackView) {
+        _headerBackView = [[CompanyHeaderView alloc]initWithFrame:CGRectMake(0, 0, kScreenSize.width, kRealSize(370))];
+    }
+    return _headerBackView;
+}
+
+- (UITableView *)jobShowTableView
+{
+    if (!_jobShowTableView) {
+        _jobShowTableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 0, kScreenSize.width, kScreenSize.height-64-49) style:UITableViewStylePlain];
+        _jobShowTableView.delegate = self;
+        _jobShowTableView.dataSource = self;
+        _jobShowTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        _jobShowTableView.estimatedRowHeight = 80;
+        _jobShowTableView.backgroundColor = [UIColor clearColor];
+        _jobShowTableView.tableHeaderView = self.headerBackView;
+    }
+    return _jobShowTableView;
+}
+
+- (CompanySubHeaderView*)subView
+{
+    if (!_subView) {
+        _subView = [[CompanySubHeaderView alloc]initWithFrame:CGRectMake(0, 0, kScreenSize.width, kRealSize(50))];
+    }
+    return _subView;
+}
+
+#pragma mark tableview delegate
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 20;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 0) {
+        UITableViewCell *cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"title"];
+        [self.subView removeFromSuperview];
+        [cell addSubview:self.subView];
+        return cell;
+    }else{
+
+        CompanyJobTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:NSStringFromClass([CompanyJobTableViewCell class])];
+        if (!cell) {
+            cell = [[CompanyJobTableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:NSStringFromClass([CompanyJobTableViewCell class])];
+        }
+        return cell;
+    }
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (indexPath.row == 0) {
+        return kRealSize(50);
+    }else{
+        return 90;
+    }
+}
+
+- (void)loadNewData
+{
+        //    NSString *path = [[NSBundle mainBundle]pathForResource:@"homeData" ofType:@"plist"];
+        //    [self.dic addObjectsFromArray:[NSArray arrayWithContentsOfFile:path]];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self.jobShowTableView.mj_header endRefreshing];
+            //        [self.orderView reloadData];
+    });
 }
 
 - (void)setupPipeline:(__kindof MIPipeline *)pipeline {
--- 51JRQ/CompanyShow/CompanyshowViewController.m
+++ 51JRQ/CompanyShow/CompanyshowViewController.m
@@ -9,18 +9,10 @@
 #import "CompanyshowViewController.h"
 #import "CompanyshowPipeline.h"
 #import "Minya.h"
-#import "TagFilterTableViewCell.h"
-#import "LMDropdownView.h"
-#import "AppDelegate.h"
 
-@interface CompanyshowViewController ()<UITableViewDelegate,UITableViewDataSource,LMDropdownViewDelegate>
+@interface CompanyshowViewController ()
 
 @property (nonatomic, strong) CompanyshowPipeline *pipeline;
-@property (nonatomic, strong) UITableView *tagTableView;
-@property (strong, nonatomic) LMDropdownView *dropdownView;
-//测试
-@property (nonatomic, strong) NSArray *allTags;
-@property (nonatomic, strong) NSMutableArray *cellInfos;
 
 @end
 
@@ -29,129 +21,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Add you own code
-    [self setNaviBarItems];
-    _allTags = @[
-                 @"AutoLayout", @"dynamically", @"calculates", @"the", @"size", @"and", @"position",
-                 @"of", @"all", @"the", @"views", @"in", @"your", @"view", @"hierarchy", @"based",
-                 @"on", @"constraints", @"placed", @"on", @"those", @"views"
-                 ];
-    _cellInfos = [NSMutableArray new];
-    for (NSInteger i = 0; i < 50; i++) {
-        [_cellInfos addObject:[_allTags subarrayWithRange:NSMakeRange(0, i % (_allTags.count + 1))]];
-    }
-}
-
-- (void)setNaviBarItems
-{
-    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc]initWithImage:[UIImage imageNamed:@"filter_image"] style:UIBarButtonItemStylePlain target:self action:@selector(showFilterView)];
 }
 
 - (void)setupPipeline:(__kindof MIPipeline *)pipeline {
     self.pipeline = pipeline;
 }
 
-- (void)showFilterView
-{
-    [self showDropDownViewFromDirection:LMDropdownViewDirectionTop];
-}
-
-- (void)showDropDownViewFromDirection:(LMDropdownViewDirection)direction
-{
-        // Init dropdown view
-    if (!self.dropdownView) {
-        self.dropdownView = [LMDropdownView dropdownView];
-        self.dropdownView.delegate = self;
-
-            // Customize Dropdown style
-        self.dropdownView.closedScale = 0.85;
-        self.dropdownView.shouldBlurContainerView = YES;
-        self.dropdownView.blurRadius = 1;
-        self.dropdownView.blackMaskAlpha = 0.1;
-        self.dropdownView.animationDuration = 0.5;
-        self.dropdownView.animationBounceHeight = 0;
-    }
-    self.dropdownView.direction = direction;
-
-        // Show/hide dropdown view
-    AppDelegate *appdel = (AppDelegate*)[UIApplication sharedApplication].delegate;
-    if ([self.dropdownView isOpen]) {
-        [self.dropdownView hide];
-    }
-    appdel.tabBarControllerConfig.tabBarController.tabBar.hidden = YES;
-    self.dropdownView.contentBackgroundColor = [UIColor blueColor];
-    [self.dropdownView showInView:self.navigationController.visibleViewController.view withContentView:self.tagTableView atOrigin:CGPointMake(0, 64)];
-}
-
-- (UITableView*)tagTableView
-{
-    if (!_tagTableView) {
-        _tagTableView = [[UITableView alloc]initWithFrame:CGRectMake(0, 64, kScreenSize.width, kScreenSize.height-64) style:UITableViewStylePlain];
-        _tagTableView.delegate = self;
-        _tagTableView.dataSource = self;
-        _tagTableView.rowHeight = UITableViewAutomaticDimension;
-        _tagTableView.estimatedRowHeight = 80;
-        _tagTableView.backgroundColor = [UIColor redColor];
-    }
-    return _tagTableView;
-}
-
-#pragma mark - Table view data source
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return _cellInfos.count;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    TagFilterTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"tagcell"];
-    if (!cell) {
-        cell = [[TagFilterTableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"tagcell"];
-    }
-    [cell setTags:_cellInfos[(NSUInteger) indexPath.row]];
-    cell.label.text = @"城市";
-
-    return cell;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    NSArray *arrays = _cellInfos[(NSUInteger) indexPath.row];
-    TTGTextTagCollectionView *view = [[TTGTextTagCollectionView alloc]initWithFrame:CGRectMake(0, 0, kScreenSize.width-30, 10)];
-    [view addTags:arrays];
-//    return 100;
-    DeBugLog(@"高度是%f",view.contentSize.height);
-    return view.contentSize.height + 30 + 10;
-}
-
-#pragma mark LMDDropView
-
-- (void)dropdownViewWillShow:(LMDropdownView *)dropdownView
-{
-    NSLog(@"Dropdown view will show");
-}
-
-- (void)dropdownViewDidShow:(LMDropdownView *)dropdownView
-{
-    NSLog(@"Dropdown view did show");
-}
-
-- (void)dropdownViewWillHide:(LMDropdownView *)dropdownView
-{
-    NSLog(@"Dropdown view will hide");
-
-}
-
-- (void)dropdownViewDidHide:(LMDropdownView *)dropdownView
-{
-    NSLog(@"Dropdown view did hide");
-    AppDelegate *appdel = (AppDelegate*)[UIApplication sharedApplication].delegate;
-    CGRect originFrame = appdel.tabBarControllerConfig.tabBarController.tabBar.frame;
-    appdel.tabBarControllerConfig.tabBarController.tabBar.frame = CGRectMake(0, originFrame.origin.y, originFrame.size.width, 0);
-    appdel.tabBarControllerConfig.tabBarController.tabBar.hidden = NO;
-    [UIView animateWithDuration:3 animations:^{
-        appdel.tabBarControllerConfig.tabBarController.tabBar.frame = originFrame;
-    }];
-
-}
 
 
 @end
